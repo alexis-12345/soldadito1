c84b2a913bc228f2a8ee11fea13b205e6ce69770
alexis-12345 <alexis2003sagba@gmail.com>
2025-05-19 17:18:25 -0500

    Update hello.R

--- R/hello.R
+++ R/hello.R
@@ -1,18 +1,12 @@
-############################################
-#####        CLASE N°  17        ##########
-## Por : BRYAN SAGBA and ANDY FIERRO   #####
-####    29-04-2025                     #####
-############################################
+n_pares <- function() {
+  n <- as.integer(readline("¿Cuántos números pares deseas ver? "))
 
-# realizar un programa que permita sumar dos numeros, usando funciones
-# Definición de la función para sumar dos números
-ingreso<-function(){
-  cat("ingrese un numero:\n")
-  A<-readLines(n=1)
-  A<-as.numeric(A)
-  return(A)
+  if (n > 0) {
+    for (i in 1:n) {
+      cat((2 * i ), "\n")
+    }
+  } else {
+    cat("Por favor ingresa un número mayor que 0.\n")
+  }
 }
-sumar2 <- function(A,B) {
-  C <- A + B
-  return(C)
-}
+n_pares()
